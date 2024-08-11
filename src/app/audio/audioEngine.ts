ce4fa990e4daac6b22007621a7f4a1b84773a043
Steve MacKinnon <mackinnon.st@gmail.com>
2024-08-11 12:50:29 -0400

    WIP implementation of tree diffing algo that updates the webaudio graph

--- src/app/audio/audioEngine.ts
+++ src/app/audio/audioEngine.ts
@@ -6,7 +6,7 @@ export interface Node {
   children?: Node[];
   id: number;
   nodeType: "osc" | "filter" | "destination";
-  props?: Object;
+  props?: any;
 }
 
 export interface OscNode extends Node {
@@ -35,7 +35,7 @@ export interface AudioState {
   nodes: NodeStore;
 }
 
-let currentRoot: Node | undefined;
+let currentRoot: Node | null = null;
 let nodeStore = new Map<number, AudioNode>();
 
 // export function updateState(root: Node) {
@@ -45,7 +45,7 @@ let nodeStore = new Map<number, AudioNode>();
 //   }
 // }
 
-function buildNode(node: Node): AudioNode | null {
+function buildAudioNode(node: Node): AudioNode | null {
   switch (node.nodeType) {
     case "osc":
       // Osc nodes are created dynamically when they are triggered by a sequence
@@ -57,26 +57,92 @@ function buildNode(node: Node): AudioNode | null {
   }
 }
 
-function buildAudioNodes(root: Node) {
-  const parent = buildNode(root);
-  if (parent) {
-    nodeStore.set(root.id, parent);
+function deleteNode(node: Node) {
+  // Remove existing node
+  if (node.children) {
+    node.children.forEach((n) => deleteNode(n));
   }
-  if (root.children) {
-    for (const childNode of root.children) {
-      const child = buildAudioNodes(childNode);
-      if (child && parent) {
-        child.connect(parent);
+  nodeStore.get(node.id)?.disconnect();
+  nodeStore.delete(node.id);
+}
+
+function buildAudioGraph(
+  newRoot: Node,
+  currentRoot: Node | null
+): AudioNode | null {
+  let shouldReplaceNode = !currentRoot || newRoot.id != currentRoot.id;
+  if (shouldReplaceNode) {
+    if (currentRoot) {
+      // Remove existing node
+      deleteNode(currentRoot);
+      // Update the ref to point to the new node
+      currentRoot.id = newRoot.id;
+      currentRoot.children = structuredClone(newRoot.children);
+      currentRoot.nodeType = newRoot.nodeType;
+      currentRoot.props = structuredClone(newRoot.props);
+    }
+
+    // Build the new audio node
+    const audioNode = buildAudioNode(newRoot);
+    if (audioNode) {
+      nodeStore.set(newRoot.id, audioNode);
+    }
+    // Rebuild all of the children
+    if (newRoot.children) {
+      for (const child of newRoot.children) {
+        const newChild = buildAudioGraph(child, null);
+        if (newChild && audioNode) {
+          newChild.connect(audioNode);
+        }
+      }
+    }
+    return audioNode;
+  }
+  // Add newly added children
+  if (newRoot.children) {
+    const parentAudioNode = nodeStore.get(newRoot.id)!;
+    for (const newChild of newRoot.children) {
+      // Look for a match of the child's id in the current child array
+      const currentChild = currentRoot?.children?.find(
+        (n) => n.id === newChild.id
+      );
+      const childAudioNode = (() => {
+        if (!currentChild) {
+          return buildAudioGraph(newChild, null);
+        } else {
+          return buildAudioGraph(newChild, currentChild);
+        }
+      })();
+      if (childAudioNode && parentAudioNode) {
+        childAudioNode.connect(parentAudioNode);
       }
     }
   }
-  return parent;
+  // Delete removed children
+  if (currentRoot && currentRoot.children) {
+    for (const childNode of currentRoot.children) {
+      // Is this node missing from the new node's child array?
+      if (
+        !newRoot.children ||
+        (newRoot.children &&
+          !newRoot.children.find((n) => n.id === childNode.id))
+      ) {
+        deleteNode(childNode);
+      }
+    }
+  }
+  const node = nodeStore.get(newRoot.id) ?? null;
+  return node;
 }
 
-export function renderAudioGraph(root: Node) {
-  buildAudioNodes(root);
-  currentRoot = root;
-  start();
+let started = false;
+export function renderAudioGraph(newRoot: Node) {
+  buildAudioGraph(newRoot, currentRoot);
+  currentRoot = structuredClone(newRoot);
+  if (!started) {
+    start();
+    started = true;
+  }
 }
 
 function instantiateGenerators(root: Node, time: number) {
--- src/app/components/Sequencer.tsx
+++ src/app/components/Sequencer.tsx
@@ -1,5 +1,6 @@
 "use client";
 
+import { ChangeEvent, useState } from "react";
 import {
   renderAudioGraph as renderAudioGraph,
   Node,
@@ -10,25 +11,89 @@ const root: Node = {
   nodeType: "destination",
   children: [
     {
-      id: 1,
-      nodeType: "osc",
+      id: 3,
+      nodeType: "filter",
       props: {
-        frequency: 200,
-        type: "sine",
+        frequency: 1000,
+        type: "lowpass",
+        q: 1.2,
       },
-      children: [],
-    },
-    {
-      id: 2,
-      nodeType: "osc",
-      props: {
-        frequency: 420,
-        type: "sawtooth",
-      },
-      children: [],
+      children: [
+        {
+          id: 1,
+          nodeType: "osc",
+          props: {
+            frequency: 200,
+            type: "sine",
+          },
+          children: [],
+        },
+        {
+          id: 2,
+          nodeType: "osc",
+          props: {
+            frequency: 420,
+            type: "sawtooth",
+          },
+          children: [],
+        },
+      ],
     },
   ],
 };
+const root2: Node = {
+  id: 0,
+  nodeType: "destination",
+  children: [
+    {
+      id: 4,
+      nodeType: "filter",
+      props: {
+        frequency: 1000,
+        type: "highpass",
+        q: 1.2,
+      },
+      children: [
+        {
+          id: 5,
+          nodeType: "osc",
+          props: {
+            frequency: 100,
+            type: "sine",
+          },
+          children: [],
+        },
+        {
+          id: 6,
+          nodeType: "osc",
+          props: {
+            frequency: 520,
+            type: "sawtooth",
+          },
+          children: [],
+        },
+      ],
+    },
+  ],
+};
+
 export function Sequencer() {
-  return <button onClick={() => renderAudioGraph(root)}>Start</button>;
+  const [lowpassFreq, setLowpassFreq] = useState(1000);
+  return (
+    <>
+      <input
+        type="range"
+        min={20}
+        max={20000}
+        onChange={(e: ChangeEvent<HTMLInputElement>) => {
+          const freq = Number.parseFloat(e.target.value);
+          root.children![0].props!.frequency = freq;
+          renderAudioGraph(root);
+        }}
+        width={300}
+      />
+      <button onClick={() => renderAudioGraph(root)}>Start</button>{" "}
+      <button onClick={() => renderAudioGraph(root2)}>Start 2</button>{" "}
+    </>
+  );
 }
