c1a254cc2d8f5ace97cdb85652b58ac885e5c238
Steve MacKinnon <mackinnon.st@gmail.com>
2024-08-17 15:18:41 -0400

    Add cursor selector component

--- src/app/components/AudioGraph.tsx
+++ src/app/components/AudioGraph.tsx
@@ -4,6 +4,7 @@ import { Oscillator } from "components/Oscillator";
 import { useAtom } from "jotai";
 import { nodesAtom } from "state";
 import { uniqueId } from "utils";
+import { CursorModeSelector } from "./CursorModeSelector";
 
 export function AudioGraph() {
   const [nodes, setNodes] = useAtom(nodesAtom);
@@ -23,13 +24,20 @@ export function AudioGraph() {
   return (
     <div
       className="absolute top-0 bottom-0 left-0 right-0 bg-indigo-950"
-      onMouseDown={(e) => addNode({ x: e.pageX, y: e.pageY })}
+      onMouseDown={(e) => {
+        if (e.target !== e.currentTarget) {
+          // Ignore mouse clicks that originated from children
+          return;
+        }
+        addNode({ x: e.pageX, y: e.pageY });
+      }}
     >
       <ul>
         {nodes.map((nodeState) => (
           <Oscillator {...nodeState} key={nodeState.id} />
         ))}
       </ul>
+      <CursorModeSelector className="absolute left-1/2 bottom-10" />
     </div>
   );
 }
--- src/app/state.ts
+++ src/app/state.ts
@@ -1,12 +1,4 @@
 import { atom } from "jotai";
-import { selectAtom } from "jotai/utils";
-
-export interface DragState {
-  id: string;
-  xOffset: number;
-  yOffset: number;
-}
-export const activeDragAtom = atom<DragState | null>(null);
 
 export interface NodeState {
   x: number;
@@ -17,11 +9,5 @@ export interface NodeState {
 
 export const nodesAtom = atom<NodeState[]>([]);
 
-export const atomForNode = (key: string) =>
-  selectAtom(nodesAtom, (nodes) => {
-    const node = nodes.find((v) => v.id === key);
-    if (!node) {
-      throw new Error(`Unable to locate node with key ${key}`);
-    }
-    return node;
-  });
+export type CursorMode = "selection" | "osc" | "filter";
+export const cursorModeAtom = atom<CursorMode>("selection");
