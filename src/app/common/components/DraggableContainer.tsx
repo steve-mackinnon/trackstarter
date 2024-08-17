666bf6fb3f71ee4671c7be7ff81ac31c1f9fecd8
Steve MacKinnon <mackinnon.st@gmail.com>
2024-08-16 23:09:14 -0400

    Split common behavior out into a DraggableContainer component

---
+++ src/app/common/components/DraggableContainer.tsx
@@ -0,0 +1,78 @@
+import { PropsWithChildren, useEffect, useState } from "react";
+
+interface DragState {
+  xOffset: number;
+  yOffset: number;
+}
+
+export function DraggableContainer({
+  x,
+  y,
+  label,
+  children,
+}: PropsWithChildren<{
+  x: number;
+  y: number;
+  label: string;
+}>) {
+  const [pos, setPos] = useState({ x, y });
+  const [dragState, setDragState] = useState<DragState | null>(null);
+  const [isMouseOver, setIsMouseOver] = useState(false);
+
+  useEffect(() => {
+    const mouseUp = (e: any) => {
+      e.stopPropagation();
+      setDragState(null);
+    };
+    window.addEventListener("mouseup", mouseUp);
+
+    const mouseMove = (e: MouseEvent) => {
+      if (dragState) {
+        setPos({
+          x: e.clientX - dragState.xOffset,
+          y: e.clientY - dragState.yOffset,
+        });
+      }
+    };
+    window.addEventListener("mousemove", mouseMove);
+    return () => {
+      window.removeEventListener("mouseup", mouseUp);
+      window.removeEventListener("mousemove", mouseMove);
+    };
+  }, [dragState, setDragState, setPos]);
+
+  const getBorderColor = () => {
+    if (dragState) {
+      return "white";
+    } else if (isMouseOver) {
+      return "yellow";
+    }
+    return "transparent";
+  };
+  return (
+    <div
+      style={{
+        transform: `translate(${pos.x}px, ${pos.y}px)`,
+        width: "200px",
+        height: "160px",
+        border: `2px solid ${getBorderColor()}`,
+        boxSizing: "border-box",
+        background: isMouseOver ? "darkturquoise" : "slategray",
+      }}
+      className={`absolute flex flex-col bg-gray-500 p-4 items-center rounded-2xl`}
+      onMouseDown={(e) => {
+        e.stopPropagation();
+        setDragState({
+          xOffset: e.clientX - pos.x,
+          yOffset: e.clientY - pos.y,
+        });
+      }}
+      onMouseUp={() => setDragState(null)}
+      onMouseEnter={() => setIsMouseOver(true)}
+      onMouseLeave={() => setIsMouseOver(false)}
+    >
+      <label className="pb-3 select-none font-bold">{label}</label>
+      {children}
+    </div>
+  );
+}
--- src/app/components/Oscillator.tsx
+++ src/app/components/Oscillator.tsx
@@ -1,70 +1,12 @@
-import { useEffect, useState } from "react";
-
-interface DragState {
-  xOffset: number;
-  yOffset: number;
-}
+import { DraggableContainer } from "common/components/DraggableContainer";
+import { useState } from "react";
 
 export function Oscillator(props: { x: number; y: number; id: string }) {
   const [frequency, setFrequency] = useState(200);
   const [type, setType] = useState<OscillatorType>("sine");
-  const [pos, setPos] = useState({ x: props.x, y: props.y });
-  const [dragState, setDragState] = useState<DragState | null>(null);
-  const [isMouseOver, setIsMouseOver] = useState(false);
 
-  useEffect(() => {
-    const mouseUp = (e: any) => {
-      e.stopPropagation();
-      setDragState(null);
-    };
-    window.addEventListener("mouseup", mouseUp);
-
-    const mouseMove = (e: MouseEvent) => {
-      if (dragState) {
-        setPos({
-          x: e.clientX - dragState.xOffset,
-          y: e.clientY - dragState.yOffset,
-        });
-      }
-    };
-    window.addEventListener("mousemove", mouseMove);
-    return () => {
-      window.removeEventListener("mouseup", mouseUp);
-      window.removeEventListener("mousemove", mouseMove);
-    };
-  }, [dragState, setDragState, setPos]);
-
-  const getBorderColor = () => {
-    if (dragState) {
-      return "white";
-    } else if (isMouseOver) {
-      return "yellow";
-    }
-    return "transparent";
-  };
   return (
-    <div
-      style={{
-        transform: `translate(${pos.x}px, ${pos.y}px)`,
-        width: "200px",
-        height: "160px",
-        border: `2px solid ${getBorderColor()}`,
-        boxSizing: "border-box",
-        background: isMouseOver ? "darkturquoise" : "slategray",
-      }}
-      className={`absolute flex flex-col bg-gray-500 p-4 items-center rounded-2xl`}
-      onMouseDown={(e) => {
-        e.stopPropagation();
-        setDragState({
-          xOffset: e.clientX - pos.x,
-          yOffset: e.clientY - pos.y,
-        });
-      }}
-      onMouseUp={() => setDragState(null)}
-      onMouseEnter={() => setIsMouseOver(true)}
-      onMouseLeave={() => setIsMouseOver(false)}
-    >
-      <label className="pb-3 select-none font-bold">Oscillator</label>
+    <DraggableContainer {...props} label="Oscillator">
       <label htmlFor="frequencySlider" className="select-none">
         Frequency
       </label>
@@ -93,6 +35,6 @@ export function Oscillator(props: { x: number; y: number; id: string }) {
         <option value="square">Square</option>
         <option value="triangle">Triangle</option>
       </select>
-    </div>
+    </DraggableContainer>
   );
 }
