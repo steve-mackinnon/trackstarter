e42615d254a04edd8e8b544b243719a21b3f0e0f
Steve MacKinnon <mackinnon.st@gmail.com>
2024-08-23 19:48:35 -0400

    Correct filter default value

--- src/app/components/Filter.tsx
+++ src/app/components/Filter.tsx
@@ -1,12 +1,15 @@
-import { setProperty } from "audio/audioGraph";
+import { FilterProps, setProperty } from "audio/audioGraph";
 import { AudioParamSlider } from "common/components/AudioParamSlider";
 import { DraggableContainer } from "common/components/DraggableContainer";
 import { useState } from "react";
 
-export function Filter(props: { x: number; y: number; nodeId: string }) {
-  const [frequency, setFrequency] = useState(1000);
+export function Filter(props: {
+  x: number;
+  y: number;
+  nodeId: string;
+  props: FilterProps;
+}) {
   const [type, setType] = useState<BiquadFilterType>("lowpass");
-  const [q, setQ] = useState(0.707);
 
   return (
     <DraggableContainer {...props} label="Filter" hasConnectionPort={true}>
@@ -32,7 +35,7 @@ export function Filter(props: { x: number; y: number; nodeId: string }) {
         id="frequency"
         min={20}
         max={20000}
-        default={1000}
+        default={props.props.frequency}
         scaling={2}
         handleValueChange={(v) =>
           setProperty(props.nodeId, "filter", "frequency", v)
@@ -44,7 +47,7 @@ export function Filter(props: { x: number; y: number; nodeId: string }) {
         min={0.1}
         max={12}
         scaling={2}
-        default={0.707}
+        default={props.props.q}
         handleValueChange={(v) => setProperty(props.nodeId, "filter", "q", v)}
       />
     </DraggableContainer>
