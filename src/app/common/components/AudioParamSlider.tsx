b852a05766ed13fafa9b9094e5e3e20cb1973eae
Steve MacKinnon <mackinnon.st@gmail.com>
2024-08-23 19:47:38 -0400

    Fix default value for param slider

--- src/app/common/components/AudioParamSlider.tsx
+++ src/app/common/components/AudioParamSlider.tsx
@@ -1,28 +1,50 @@
 import { useState } from "react";
 
 function scaleValue({
-  current,
+  normalizedValue,
   min,
   max,
   scaling,
 }: {
-  current: number;
+  normalizedValue: number;
   min: number;
   max: number;
   scaling: number;
 }) {
-  const scaled = Math.pow(current, scaling);
+  const scaled = Math.pow(normalizedValue, scaling);
   return min + scaled * (max - min);
 }
 
-function formatValue(value: number, maxValue: number): string {
-  return value.toFixed(0);
-  if (value >= 1000) {
-    const v = value / 1000;
-    const decimals = value >= 10000 ? 1 : 2;
-    return `${v.toFixed(decimals)}k`;
+function inverseScaleValue({
+  value,
+  min,
+  max,
+  scaling,
+}: {
+  value: number;
+  min: number;
+  max: number;
+  scaling: number;
+}) {
+  const normalizedValue = (value - min) / (max - min);
+  return Math.pow(normalizedValue, 1 / scaling);
+}
+
+function formatValue({
+  value,
+  maxValue,
+  abbreviateLargeValues,
+}: {
+  value: number;
+  maxValue: number;
+  abbreviateLargeValues: boolean;
+}): string {
+  if (!abbreviateLargeValues || value < 1000) {
+    return value.toFixed(maxValue < 100 ? 3 : 0);
   }
-  return value.toFixed(maxValue < 100 ? 3 : 0);
+  const v = value / 1000;
+  const decimals = value >= 10000 ? 1 : 2;
+  return `${v.toFixed(decimals)}k`;
 }
 
 interface SliderProps {
@@ -32,15 +54,23 @@ interface SliderProps {
   min: number;
   max: number;
   default: number;
-  // 1 = Linear
-  // > 1 = Exponential
-  // < 1 = Logarithmic
+  /// 1 = Linear
+  /// > 1 = Exponential
+  /// < 1 = Logarithmic
   scaling: number;
+  abbreviateLargeValues?: boolean;
 }
 
 export function AudioParamSlider(props: SliderProps) {
-  // TODO: map real value to normalized
-  const [normalizedValue, setNormalizedValue] = useState(0.2);
+  const normalizedDefaultValue = inverseScaleValue({
+    value: props.default,
+    min: props.min,
+    max: props.max,
+    scaling: props.scaling,
+  });
+  const [normalizedValue, setNormalizedValue] = useState(
+    normalizedDefaultValue
+  );
   const [value, setValue] = useState(props.default);
 
   return (
@@ -55,7 +85,7 @@ export function AudioParamSlider(props: SliderProps) {
         onChange={(e) => {
           const v = Number.parseFloat(e.target.value);
           const scaled = scaleValue({
-            current: v,
+            normalizedValue: v,
             min: props.min,
             max: props.max,
             scaling: props.scaling,
@@ -72,7 +102,11 @@ export function AudioParamSlider(props: SliderProps) {
           {props.label}
         </label>
         <output htmlFor={props.id} className="select-none">
-          {formatValue(value, props.max)}
+          {formatValue({
+            value,
+            maxValue: props.max,
+            abbreviateLargeValues: props.abbreviateLargeValues ?? true,
+          })}
         </output>
       </div>
     </div>
