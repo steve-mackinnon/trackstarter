47b2f48d0ed4163aa961daec04eb6d64ea3669ad
Steve MacKinnon <mackinnon.st@gmail.com>
2024-08-25 14:26:37 -0400

    Add chord progressions for the various moods

--- src/app/audio/sequenceGenerator.ts
+++ src/app/audio/sequenceGenerator.ts
@@ -1,57 +1,174 @@
 import { Chord, NoteWithOctave, Scale, Note as TNote } from "tonal";
 import { SequencerEvent } from "./audioGraph";
 
-export type Mood = "Uplifting" | "Dark" | "Exotic"; // | "Mysterious"
-// | "Dramatic"
-// | "Sophisticated"
-// | "Dreamy"
-// | "Groovy"
-// | "Surreal";
+export type Mood =
+  | "Uplifting"
+  | "Dark"
+  | "Exotic"
+  | "Mysterious"
+  | "Dramatic"
+  | "Sophisticated"
+  | "Dreamy"
+  | "Groovy"
+  | "Surreal";
 
 const MOOD_TO_SCALE: Record<Mood, string> = {
   Dark: "aeolian",
-  // Dramatic: "harmonic minor",
-  // Dreamy: "lydian",
+  Dramatic: "harmonic minor",
+  Dreamy: "lydian",
   Exotic: "phrygian",
-  // Groovy: "mixolydian",
-  // Mysterious: "whole tone",
-  // Sophisticated: "melodic minor",
-  // Surreal: "augmented",
+  Groovy: "mixolydian",
+  Mysterious: "whole tone",
+  Sophisticated: "melodic minor",
+  Surreal: "augmented",
   Uplifting: "ionian",
 };
 
-const MOOD_TO_PROGRESSIONS: Record<Mood, string[]> = {
-  Dark: ["1-4-1-5", "1-6-1-7", "1-7-6-7", "1-6-3-7"],
+export const MOOD_TO_PROGRESSIONS = {
   Uplifting: [
     "1-5-6-4",
-    "1-4-5-1",
+    "1-4-5-4",
+    "1-5-4-5",
     "1-6-4-5",
-    "1-4-6-5",
+    "1-4-2-5",
+    "1-5-3-4",
+    "1-2-5-1",
+    "4-1-5-1",
+    "1-3-4-5",
+    "1-4-5-1",
+  ],
+  Dark: [
+    "1-b6-b7-5",
+    "1-4-b6-5",
+    "1-b2-4-5",
+    "6-5-4-3",
+    "1-3-b6-b2",
+    "1-b3-b7-b6",
+    "1-b5-4-b2",
+    "1-b7-b6-5",
+    "1-b7-b6-4",
+    "1-b3-4-5",
+  ],
+  Exotic: [
+    "1-b2-5-1",
+    "1-4-b2-5",
+    "1-b3-4-1",
+    "1-2-5-b2",
+    "1-b7-b2-1",
+    "1-b5-5-1",
+    "1-b2-b3-1",
+    "1-#4-5-1",
+    "1-4-b7-1",
+    "1-b5-b7-1",
+  ],
+  Mysterious: [
+    "1-b6-4-b7",
+    "1-b7-b6-5",
+    "1-5-b6-3",
+    "1-b2-1-5",
+    "1-4-b7-3",
+    "1-b7-1-b7",
+    "b6-b7-1",
+    "1-b7-b6-5",
+    "1-b6-3-b7",
+    "1-4-2-b7",
+  ],
+  Dramatic: [
+    "1-5-6-4",
+    "1-4-5-1",
+    "6-4-1-5",
+    "1-b7-b6-5",
+    "1-5-4-b7",
+    "1-b7-4-5",
+    "6-5-4-5",
+    "1-b7-4-1",
+    "1-4-5-4",
+    "1-5-4-1",
+  ],
+  Sophisticated: [
+    "2-5-1-4",
+    "1-2-4-b7",
+    "4-5-3-6",
+    "2-5-1-b7",
+    "1-5-2-4",
+    "1-#4-4-3",
+    "2-5-4-b3",
+    "1-6-2-5",
+    "1-4-b7-3",
+    "1-b2-3-4",
+  ],
+  Dreamy: [
+    "1-5-4-5",
+    "1-6-4-5",
+    "1-2-5-4",
+    "1-5-6-5",
+    "4-1-5-1",
     "6-4-1-5",
     "1-5-4-5",
-    "1-4-1-5",
+    "1-4-b7-5",
+    "1-4-b7-4",
+    "1-2-5-1",
   ],
-  Exotic: ["1-5-6-4", "1-4-5-1", "1-2-3-7"],
-};
+  Groovy: [
+    "1-7-4-5",
+    "1-6-2-5",
+    "1-4-b7-1",
+    "1-5-2-6",
+    "1-7-6-5",
+    "1-4-5-4",
+    "1-b3-4-5",
+    "1-b7-4-b6",
+    "1-5-6-5",
+    "1-b7-4-5",
+  ],
+  Surreal: [
+    "1-b2-4-b6",
+    "1-b5-4-b7",
+    "1-#4-5-#5",
+    "1-b2-5-1",
+    "1-b3-4-b5",
+    "1-#4-4-5",
+    "1-4-b6-b2",
+    "1-b6-b3-b7",
+    "1-#5-4-5",
+    "1-b7-b6-b5",
+  ],
+} as const;
 
 const MOODS: Mood[] = [
   "Uplifting",
   "Dark",
-  // "Dramatic",
-  // "Dreamy",
+  "Dramatic",
+  "Dreamy",
   "Exotic",
-  // "Groovy",
-  // "Mysterious",
-  // "Sophisticated",
-  // "Surreal",
+  "Groovy",
+  "Mysterious",
+  "Sophisticated",
+  "Surreal",
 ];
 
-function getRandomValue<T>(array: T[]): T {
+function getRandomValue<T>(array: readonly T[]): T {
   return array[Math.floor(Math.random() * (array.length - 1))];
 }
 
-function convertChordProgression(progression: string): number[] {
-  return progression.split("-").map((chord) => parseInt(chord, 10) - 1);
+interface ScaleDegree {
+  index: number;
+  alteration?: "sharp" | "flat";
+}
+
+function parseChordProgression(progression: string): ScaleDegree[] {
+  return progression.split("-").map((chord) => {
+    const alteration = chord.startsWith("b")
+      ? "flat"
+      : chord.startsWith("#")
+      ? "sharp"
+      : undefined;
+    const skipFirstChar = alteration !== undefined;
+    return {
+      index: parseInt(chord.slice(skipFirstChar ? 1 : 0), 10) - 1,
+      alteration,
+    };
+  });
 }
 
 export function getRandomMood(): Mood {
@@ -129,7 +246,7 @@ export function generateChordProgression({
   const scale = MOOD_TO_SCALE[mood];
   const notes = Scale.get(`${rootNote}${octave} ${scale}`);
   const progression = getRandomValue(MOOD_TO_PROGRESSIONS[mood]);
-  const chordDegrees = convertChordProgression(
+  const chordDegrees = parseChordProgression(
     getRandomValue(MOOD_TO_PROGRESSIONS[mood])
   );
   const chordNotes = chordDegrees.map((degree) =>
@@ -163,11 +280,16 @@ export function chordProgressionToSequencerEvents(
 
 function chordForScale(
   scale: Scale.Scale,
-  rootDegree: number,
+  rootDegree: ScaleDegree,
   numNotesInChord: number
 ): string[] {
   return Array.from({ length: numNotesInChord }).map((_, i) => {
-    let note = scale.notes[(rootDegree + i * 2) % scale.notes.length];
+    let note = scale.notes[(rootDegree.index + i * 2) % scale.notes.length];
+    if (rootDegree.alteration && i === 0) {
+      // Sharpen or flatten the root if requested
+      const shiftAmount = rootDegree.alteration === "flat" ? "-1m" : "1m";
+      note = TNote.transpose(note, shiftAmount);
+    }
     const freq = TNote.get(note).freq ?? 500;
     if (Math.random() < 0.3 && freq < 700) {
       // Shift up an octave
