7d2012a6995943a7d6244ee5edb555c302199638
Steve MacKinnon <mackinnon.st@gmail.com>
2024-08-29 13:15:19 -0400

    Add theme provider

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,6 +1,6 @@
+import { ThemeProvider } from "components/ThemeProvider";
 import type { Metadata } from "next";
 import { Inter } from "next/font/google";
-import { ThemeProvider } from "components/ThemeProvider";
 import "./globals.css";
 
 const inter = Inter({ subsets: ["latin"] });
