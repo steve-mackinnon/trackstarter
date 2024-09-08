f80602d45037d8a184b74163a00687dddf78b2f2
Steve MacKinnon <mackinnon.st@gmail.com>
2024-09-08 12:57:35 -0400

    More efficient soft clip fn

--- public/audio/masterClipper.js
+++ public/audio/masterClipper.js
@@ -7,16 +7,14 @@ class MasterClipperProcessor extends AudioWorkletProcessor {
     const input = inputs[0];
     const output = outputs[0];
 
-    const a = 4.4;
+    const a = 19;
     for (let channel = 0; channel < input.length; channel++) {
       const inputChannel = input[channel];
       const outputChannel = output[channel];
       for (let i = 0; i < inputChannel.length; i++) {
         const sample = inputChannel[i];
-        // f(x) = sgn(x) * tanh((x^2)^a/2)^1/a
-        outputChannel[i] =
-          Math.sign(sample) *
-          Math.pow(Math.tanh(Math.pow(sample * sample, a / 2)), 1 / a);
+        const x = Math.min(1, Math.max(sample, -1));
+        outputChannel[i] = x - (1 / a) * Math.pow(x, a);
       }
     }
     return true;
