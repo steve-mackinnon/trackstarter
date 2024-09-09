513470d7a6d18185d40c48ad2506541e84ca21db
Steve MacKinnon <mackinnon.st@gmail.com>
2024-09-09 12:49:57 -0400

    When generating a melody, generate 5 options and pick the one with the largest note diversity

--- src/app/audio/melodyGenerator.ts
+++ src/app/audio/melodyGenerator.ts
@@ -17,16 +17,28 @@ export async function generateMelodyForChordProgression(
       await mvae.initialize();
       initialized = true;
     }
-    // MusicVAE can't handle slash chords, so just omit them
+    // MusicVAE can't handle slash chords, so just omit the slash and rely on snapping
+    // to scale to keep the melody in key.
     chordProgression = chordProgression.map((chord) => chord.split("/")[0]);
-    const NUM_SAMPLES = 1;
+    const NUM_SAMPLES = 5;
     const TEMP = 0.5;
     const sequences = await mvae.sample(NUM_SAMPLES, TEMP, {
       chordProgression,
     });
     const scale = Scale.get(`${rootNote} ${scaleName}`).notes;
-    const mappedSequence: SequencerEvent[] | undefined =
-      sequences[0].notes?.map((ns) => {
+    // Choose the melodity with the largest variety of notes
+    const sequence = sequences.sort((a, b) => {
+      const aUniqueNotes = new Set(a.notes!.map((n) => n.pitch!)).size;
+      const bUniqueNotes = new Set(b.notes!.map((n) => n.pitch!)).size;
+      if (aUniqueNotes < bUniqueNotes) {
+        return -1;
+      } else if (aUniqueNotes === bUniqueNotes) {
+        return 0;
+      }
+      return 1;
+    })[0];
+    const mappedSequence: SequencerEvent[] | undefined = sequence.notes?.map(
+      (ns) => {
         const note = Midi.midiToNoteName(ns.pitch as number);
         const octave = note[note.length - 1];
         const noteSnappedToScale = scale
@@ -44,7 +56,8 @@ export async function generateMelodyForChordProgression(
           startStep: (ns.quantizedStartStep as number) * 2,
           endStep: (ns.quantizedEndStep as number) * 2,
         };
-      });
+      },
+    );
     return mappedSequence;
   } catch (e) {
     console.error(e);
