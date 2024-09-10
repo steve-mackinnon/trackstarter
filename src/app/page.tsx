52144a0d378db5fbdd3f4402e5b3dce1f06e3a4b
Steve MacKinnon <mackinnon.st@gmail.com>
2024-09-10 13:30:48 -0400

    Font tweaks on landing page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,6 +1,11 @@
 import { Button } from "common/components/ui/button";
+import { cn } from "common/utils";
 import type { Metadata } from "next";
+import { Orbitron, Roboto_Flex } from "next/font/google";
 import Link from "next/link";
+
+const orbitron = Orbitron({ subsets: ["latin"] });
+const roboto = Roboto_Flex({ subsets: ["latin"] });
 
 export const metadata: Metadata = {
   title: "Track Starter",
@@ -23,18 +28,28 @@ export const metadata: Metadata = {
 
 export default function Home() {
   return (
-    <main className="flex flex-col justify-center items-center gap-6 p-4 text-center">
-      <h1 className="text-2xl font-bold app-title">Track Starter</h1>
-      <article className="max-w-2xl">
+    <main
+      className={cn(
+        roboto.className,
+        "flex flex-col justify-center items-center gap-6 p-4 text-center",
+      )}
+    >
+      <h1 className={cn(orbitron.className, "text-4xl font-bold")}>
+        Track Starter
+      </h1>
+      <article className="max-w-2xl text-lg">
         Track Starter is an AI-powered music creation tool that generates short
         melodic ideas that can be downloaded as MIDI. I built this tool to find
         interesting chord progressions to use when writing music.
       </article>
       <section aria-labelledby="features-heading">
-        <h2 className="text-xl font-bold" id="features-heading">
+        <h2
+          className={cn(orbitron.className, "text-3xl font-bold")}
+          id="features-heading"
+        >
           Features
         </h2>
-        <ul className="list-disc text-left pl-8 pt-4">
+        <ul className="list-disc text-left pl-8 pt-4 text-lg">
           <li>
             Chord progression and melody generation powered by
             <span> </span>
@@ -56,7 +71,7 @@ export default function Home() {
       <Link href={"/workstation"}>
         <Button
           variant={"outline"}
-          className="bg-slate-900 border-2"
+          className="bg-slate-900 border-2 text-2xl w-40 h-16 rounded-xl"
           style={{
             borderColor: "var(--harmony-border-active)",
           }}
