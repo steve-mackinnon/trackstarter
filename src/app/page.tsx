aad3f7ed9bc5c7bdd314f70ea2a5421faaac8448
Steve MacKinnon <mackinnon.st@gmail.com>
2024-09-10 13:50:10 -0400

    Use Orbitron as the global font

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,55 +1,29 @@
 import { Button } from "common/components/ui/button";
 import { cn } from "common/utils";
-import type { Metadata } from "next";
-import { Orbitron, Roboto_Flex } from "next/font/google";
+import { Roboto_Flex } from "next/font/google";
 import Link from "next/link";
 
-const orbitron = Orbitron({ subsets: ["latin"] });
 const roboto = Roboto_Flex({ subsets: ["latin"] });
-
-export const metadata: Metadata = {
-  title: "Track Starter",
-  applicationName: "Track Starter",
-  description:
-    "Music creation tool that generates short melodic ideas you can download as MIDI to inspire your next song.",
-  keywords: [
-    "music",
-    "production",
-    "ai",
-    "gen",
-    "midi",
-    "generative",
-    "synthesizer",
-    "webaudio",
-    "audio",
-    "electronic",
-  ],
-};
 
 export default function Home() {
   return (
-    <main
-      className={cn(
-        roboto.className,
-        "flex flex-col justify-center items-center gap-6 p-4 text-center",
-      )}
-    >
-      <h1 className={cn(orbitron.className, "text-4xl font-bold")}>
-        Track Starter
-      </h1>
-      <article className="max-w-2xl text-lg">
+    <main className="flex flex-col justify-center items-center gap-6 p-4 pt-10 text-center">
+      <h1 className="text-4xl font-bold">Track Starter</h1>
+      <article className={cn(roboto.className, "max-w-2xl text-lg")}>
         Track Starter is an AI-powered music creation tool that generates short
         melodic ideas that can be downloaded as MIDI. I built this tool to find
         interesting chord progressions to use when writing music.
       </article>
       <section aria-labelledby="features-heading">
-        <h2
-          className={cn(orbitron.className, "text-3xl font-bold")}
-          id="features-heading"
-        >
+        <h2 className="text-3xl font-bold" id="features-heading">
           Features
         </h2>
-        <ul className="list-disc text-left pl-8 pt-4 text-lg">
+        <ul
+          className={cn(
+            `${roboto.className} antialiased`,
+            "list-disc text-left pl-8 pt-4 text-lg",
+          )}
+        >
           <li>
             Chord progression and melody generation powered by
             <span> </span>
@@ -71,7 +45,7 @@ export default function Home() {
       <Link href={"/workstation"}>
         <Button
           variant={"outline"}
-          className="bg-slate-900 border-2 text-2xl w-40 h-16 rounded-xl"
+          className="bg-slate-900 border-2 text-2xl w-48 h-16 rounded-xl"
           style={{
             borderColor: "var(--harmony-border-active)",
           }}
