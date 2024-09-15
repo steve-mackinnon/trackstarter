861b91e0aa8435a18af4f0910488fc32873acbc8
Steve MacKinnon <mackinnon.st@gmail.com>
2024-09-14 20:22:36 -0400

    Setup next bundle analyzer

---
+++ next.config.js
@@ -0,0 +1,9 @@
+/** @type {import('next').NextConfig} */
+
+const withBundleAnalyzer = require("@next/bundle-analyzer")({
+  enabled: process.env.ANALYZE === "true",
+});
+
+const nextConfig = {};
+
+module.exports = withBundleAnalyzer(nextConfig);
