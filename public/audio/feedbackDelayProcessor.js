a67fd689f2a73150f5dc38e7982fa795fdb8c27a
Steve MacKinnon <mackinnon.st@gmail.com>
2024-09-19 21:42:48 -0400

    Proper multichannel support in FeedbackDelayProcessor

--- public/audio/feedbackDelayProcessor.js
+++ public/audio/feedbackDelayProcessor.js
@@ -4,6 +4,63 @@ function lerp(a, b, t) {
 
 const MAX_DELAY_TIME = 1;
 
+class MonoDelayProcessor {
+  constructor() {
+    this.delayBuffer = new Float32Array(sampleRate * MAX_DELAY_TIME);
+    this.writeIndex = 0;
+    this.previousDelayTimeSamples = undefined;
+  }
+
+  process(inputChannel, outputChannel, delayTimeParam, feedbackParam) {
+    for (let i = 0; i < inputChannel.length; i++) {
+      const delayTime =
+        delayTimeParam.length > 1 ? delayTimeParam[i] : delayTimeParam[0];
+      const feedback =
+        feedbackParam.length > 1 ? feedbackParam[i] : feedbackParam[0];
+
+      // Calculate the delay time in samples, and interpolate between old and new delay time
+      const delayInSamples = delayTime * sampleRate;
+      const previousDelayInSamples =
+        this.previousDelayTimeSamples ?? delayInSamples;
+
+      const interpolatedDelayInSamples = lerp(
+        previousDelayInSamples,
+        delayInSamples,
+        0.0001,
+      );
+
+      const firstSampleIndex = this.getReadIndex(interpolatedDelayInSamples);
+      const secondSampleIndex =
+        (firstSampleIndex + 1) % this.delayBuffer.length;
+      const secondSampleWeight =
+        interpolatedDelayInSamples - Math.floor(interpolatedDelayInSamples);
+      const delayedSample = lerp(
+        this.delayBuffer[firstSampleIndex],
+        this.delayBuffer[secondSampleIndex],
+        secondSampleWeight,
+      );
+
+      // Write the current sample into the delay buffer with feedback
+      this.delayBuffer[this.writeIndex] =
+        inputChannel[i] + delayedSample * feedback;
+
+      // Output the delayed sample
+      outputChannel[i] = delayedSample;
+
+      // Update the write index
+      this.writeIndex = (this.writeIndex + 1) % this.delayBuffer.length;
+      this.previousDelayTimeSamples = interpolatedDelayInSamples;
+    }
+  }
+
+  getReadIndex(delayInSamples) {
+    return (
+      (this.writeIndex - Math.floor(delayInSamples) + this.delayBuffer.length) %
+      this.delayBuffer.length
+    );
+  }
+}
+
 class FeedbackDelayProcessor extends AudioWorkletProcessor {
   static get parameterDescriptors() {
     return [
@@ -24,9 +81,7 @@ class FeedbackDelayProcessor extends AudioWorkletProcessor {
 
   constructor() {
     super();
-    this.delayBuffer = new Float32Array(sampleRate * MAX_DELAY_TIME);
-    this.writeIndex = 0;
-    this.previousDelayTimeSamples = undefined;
+    this.monoProcessors = [new MonoDelayProcessor(), new MonoDelayProcessor()];
   }
 
   process(inputs, outputs, parameters) {
@@ -38,56 +93,16 @@ class FeedbackDelayProcessor extends AudioWorkletProcessor {
     for (let channel = 0; channel < input.length; channel++) {
       const inputChannel = input[channel];
       const outputChannel = output[channel];
+      const monoProcessor = this.monoProcessors[channel];
 
-      for (let i = 0; i < inputChannel.length; i++) {
-        const delayTime =
-          delayTimeParam.length > 1 ? delayTimeParam[i] : delayTimeParam[0];
-        const feedback =
-          feedbackParam.length > 1 ? feedbackParam[i] : feedbackParam[0];
-
-        // Calculate the delay time in samples, and interpolate between old and new delay time
-        const delayInSamples = delayTime * sampleRate;
-        const previousDelayInSamples =
-          this.previousDelayTimeSamples ?? delayInSamples;
-
-        const interpolatedDelayInSamples = lerp(
-          previousDelayInSamples,
-          delayInSamples,
-          0.0001,
-        );
-
-        const firstSampleIndex = this.getReadIndex(interpolatedDelayInSamples);
-        const secondSampleIndex =
-          (firstSampleIndex + 1) % this.delayBuffer.length;
-        const secondSampleWeight =
-          interpolatedDelayInSamples - Math.floor(interpolatedDelayInSamples);
-        const delayedSample = lerp(
-          this.delayBuffer[firstSampleIndex],
-          this.delayBuffer[secondSampleIndex],
-          secondSampleWeight,
-        );
-
-        // Write the current sample into the delay buffer with feedback
-        this.delayBuffer[this.writeIndex] =
-          inputChannel[i] + delayedSample * feedback;
-
-        // Output the delayed sample
-        outputChannel[i] = delayedSample;
-
-        // Update the write index
-        this.writeIndex = (this.writeIndex + 1) % this.delayBuffer.length;
-        this.previousDelayTimeSamples = interpolatedDelayInSamples;
-      }
+      monoProcessor.process(
+        inputChannel,
+        outputChannel,
+        delayTimeParam,
+        feedbackParam,
+      );
     }
-
     return true;
-  }
-
-  getReadIndex(delayInSamples) {
-    return (
-      (this.writeIndex - Math.floor(delayInSamples) + this.delayBuffer.length) %
-      this.delayBuffer.length
-    );
   }
 }
 
