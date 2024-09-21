d86f2ceba74c2732b9f88bc228e4935f2785c7f7
Steve MacKinnon <mackinnon.st@gmail.com>
2024-09-21 14:17:09 -0400

    use unmute to allow ios audio without the mute switch turned off

--- src/app/common/audio.ts
+++ src/app/common/audio.ts
@@ -1,4 +1,8 @@
 import { AudioGraph } from "audio/graph";
+import { unmute } from "audio/unmute";
 import { WebAudioDelegate } from "audio/webAudioDelegate";
+import { AudioContext } from "standardized-audio-context";
 
-export const audioGraph = new AudioGraph(new WebAudioDelegate());
+const context = new AudioContext();
+unmute(context, false, false);
+export const audioGraph = new AudioGraph(new WebAudioDelegate(context));
