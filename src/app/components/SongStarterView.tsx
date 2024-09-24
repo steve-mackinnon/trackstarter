97ada8f1c7df51894aec6fd9835ce50c43929602
Steve MacKinnon <mackinnon.st@gmail.com>
2024-09-24 13:38:42 -0400

    Fix play button positioning

--- src/app/components/SongStarterView.tsx
+++ src/app/components/SongStarterView.tsx
@@ -6,9 +6,9 @@ import { useListenToPlaybackStateChanges } from "hooks/useListenToPlaybackStateC
 import { useSetupHotkeys } from "hooks/useSetupHotkeys";
 import { useEffect } from "react";
 import { DrumControls } from "./DrumControls";
-import { Footer } from "./Footer";
 import { Header } from "./Header";
 import { InstrumentSelectionContainer } from "./InstrumentSelectionContainer";
+import { TransportButton } from "./TransportButton";
 import { XYPadContainer } from "./XYPadContainer";
 
 export default function SongStarterView() {
@@ -22,13 +22,14 @@ export default function SongStarterView() {
   }, []);
 
   return (
-    <div className="absolute flex flex-col gap-y-4 w-full h-full py-20  items-center">
+    <div className="absolute flex flex-col gap-y-2 sm:gap-y-4 w-full h-full py-20  items-center">
       <Header className="absolute top-4" />
       <XYPadContainer />
       <InstrumentSelectionContainer />
-      <div className="flex gap-x-4 sm:gap-x-0 items-center justify-between">
+      <div className="flex gap-x-14 sm:gap-x-0 items-center sm:justify-center justify-between">
+        <div></div>
         <DrumControls />
-        <Footer className="sm:absolute sm:bottom-6" />
+        <TransportButton className="sm:absolute sm:bottom-2 self-end" />
       </div>
     </div>
   );
