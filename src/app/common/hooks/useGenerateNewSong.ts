a3103798d6c0517428e4f4ede3796b10d1b2d887
Steve MacKinnon <mackinnon.st@gmail.com>
2024-09-24 19:32:34 -0400

    Fix bug where drums weren't rendered on first load

--- src/app/common/hooks/useGenerateNewSong.ts
+++ src/app/common/hooks/useGenerateNewSong.ts
@@ -43,7 +43,7 @@ export function useGenerateNewSong() {
       }
       const { kicks, snares, closedHihats, openHihats } =
         await generateDrumPattern();
-      const drums = {
+      drums = {
         muted: false,
         kickPattern: kicks,
         snarePattern: snares,
